91be26f9516b8bb05435324bb4bbb204c12614cb
Cutean Bogdan Mihai <bogdancutean@proton.me>
2025-01-07 15:09:16 +0200

    Add - template for commands

--- app.ts
+++ app.ts
@@ -13,4 +13,27 @@ program
     );
   });
 
+program
+  .command("list")
+  .description("See list of expenses")
+  .action(() => {
+    console.log("All Expenses");
+  });
+
+program
+  .command("summary")
+  .description("Sum of all expenses")
+  .option("--month <month>", "Month for expenses")
+  .action((options) => {
+    console.log("Sum of expenses");
+  });
+
+program
+  .command("delete")
+  .description("Remove an expense")
+  .option("--id <id>", "Id of expense")
+  .action((options) => {
+    console.log("Removed:" + options.id);
+  });
+
 program.parse();
